52513206d5ec5aa67a95af79f552897a5449791b
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-01-31 15:25:07 +0500

    feat(server): register ticket routes and user middleware

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
 import express from "express";
 import { mongoConnect } from "./util/database";
 
+import ticketRouter from "./routes/ticket";
 const app = express();
+
+//req.body parser
+app.use(express.json());
+
+//middleware to add user to req.user
+app.use((req, res, next) => {
+  req.user = {
+    _id: "697d0c92a8ad386d3970da2f",
+    name: "Ahmad Aamir",
+    email: "test@email.com",
+  };
+  next();
+});
+
+//Routes
+app.use("/api/tickets", ticketRouter);
 
 app.use("/", (req, res, next) => {
   res.json({
