20878a53fc47ed5dbc56f435ecc5a78c2651b043
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-01 15:43:11 +0500

    Add static methods for role verification and admin promotion in Team model

--- server/models/teams.js
+++ server/models/teams.js
@@ -21,4 +21,38 @@ export class Team {
     console.log("Team created");
     return result;
   }
+
+  static async isAdmin(teamId, userId) {
+    const db = getDb();
+    const res = await db.collection("teams").findOne({
+      _id: new ObjectId(teamId),
+      admins: new ObjectId(userId),
+    });
+
+    console.log("isAdmin :", !!res);
+    return !!res;
+  }
+
+  static async isMember(teamId, userId) {
+    const db = getDb();
+    const res = await db.collection("teams").findOne({
+      _id: new ObjectId(teamId),
+      members: new ObjectId(userId),
+    });
+
+    console.log("isMember :", !!res);
+    return !!res;
+  }
+
+  static async promoteToAdmin(teamId, userId) {
+    const db = getDb();
+    const updatedTeam = await db.collection("teams").findOneAndUpdate(
+      { _id: new ObjectId(teamId), members: new ObjectId(userId) },
+      {
+        $addToSet: { admins: new ObjectId(userId) },
+      },
+    );
+    console.log("updated team");
+    return updatedTeam;
+  }
 }
