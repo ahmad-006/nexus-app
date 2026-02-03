0d83008bb457654754be4a155acb1b56e9732e7e
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-03 12:00:00 +0500

    feat: implement Mongoose User schema

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: [8, "A password must be minimum of 8 character"],
+  },
+  image: String,
+  teams: [
+    {
+      teamId: {
+        type: Types.ObjectId,
+        ref: "Team",
+        required: true,
+      },
+      Role: {
+        type: String,
+        enum: ["Admin", "Member"],
+        Default: "Member",
+      },
+    },
+  ],
+});
+
+export const User = mongoose.model("User", userSchema);
