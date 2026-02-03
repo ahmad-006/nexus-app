9a0ccf3b3c75f4b89155d173466e7ea9022c40c4
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-03 15:00:00 +0500

    refactor: register user routes and auth middleware

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { mongoConnect } from "./util/database";
+import { mongooseConnect } from "./util/database";
 import cors from "cors";
 
 import ticketRouter from "./routes/ticket";
 import { teamsRouter } from "./routes/team";
+import { userRouter } from "./routes/user";
+import { User } from "./models/user";
 const app = express();
 
 //req.body parser
@@ -11,26 +13,22 @@ app.use(express.json());
 app.use(cors());
 
 //middleware to add user to req.user
-app.use((req, res, next) => {
-  req.user = {
-    _id: "697d0c92a8ad386d3970da2f",
-    name: "Ahmad Aamir",
-    email: "test@email.com",
-  };
+app.use(async (req, res, next) => {
+  const user = await User.findById("6981f1014856de3ab98aff07");
+  req.user = user;
   next();
 });
 
 //Routes
 app.use("/api/teams", teamsRouter);
 app.use("/api/tickets", ticketRouter);
+app.use("/api/users", userRouter);
 
 app.use("/", (req, res, next) => {
   res.json({
     message: "hello from server",
   });
 });
-
-mongoConnect(() => {
-  console.log("DB CONNECTED SUCCESSFULLY");
+mongooseConnect(() => {
   app.listen(8000);
 });
