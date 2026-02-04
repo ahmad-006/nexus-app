9fe26e5fb966312f7fdefc858b1c15f0f3305af3
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-04 10:00:00 +0500

    refactor: migrate Ticket model to Mongoose schema

--- server/models/ticket.js
+++ server/models/ticket.js
@@ -1,70 +1,108 @@
-import { ObjectId } from "mongodb";
-import { getDb } from "../util/database";
+import mongoose, { Schema, Types } from "mongoose";
 
-export class Ticket {
-  constructor(title, description, priority, reporterId, teamId) {
-    this.title = title;
-    this.description = description;
-    this.status = "TODO";
-    this.priority = priority;
-    this.reporterId = new ObjectId(reporterId);
-    this.teamId = new ObjectId(teamId);
-  }
+const ticketSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["TODO", "IN_PROGRESS", "DONE"],
+    required: false,
+  },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    required: true,
+  },
+  reporterId: {
+    type: Types.ObjectId,
+    ref: "User",
+    requuired: true,
+  },
+  assigneeId: {
+    type: Types.ObjectId,
+    ref: "User",
+    requuired: false,
+  },
+  teamId: {
+    type: Types.ObjectId,
+    ref: "Team",
+    requuired: false,
+  },
+});
 
-  save() {
-    const db = getDb();
+export const Ticket = mongoose.model("Ticket", ticketSchema);
 
-    return db
-      .collection("tickets")
-      .insertOne(this)
-      .then((result) => {
-        console.log("Ticket Added successfully");
-        return { _id: result.insertedId, ...this };
-      });
-  }
+// export class Ticket {
+//   constructor(title, description, priority, teamId, assignee, reporterId) {
+//     this.title = title;
+//     this.description = description;
+//     this.status = "TODO";
+//     this.priority = priority;
+//     this.reporterId = new ObjectId(reporterId);
+//     this.teamId = new ObjectId(teamId);
+//     this.assignee = assignee;
+//   }
 
-  static async fetchAll(teamId) {
-    try {
-      const db = getDb();
+//   save() {
+//     const db = getDb();
 
-      return await db
-        .collection("tickets")
-        .find({ teamId: new ObjectId(teamId) })
-        .toArray();
-    } catch (err) {
-      console.error("Database Fetch Error:", err);
-      throw err;
-    }
-  }
+//     return db
+//       .collection("tickets")
+//       .insertOne(this)
+//       .then((result) => {
+//         console.log("Ticket Added successfully");
+//         return { _id: result.insertedId, ...this };
+//       });
+//   }
 
-  static DeleteById(id) {
-    const db = getDb();
+//   static async fetchAll(teamId) {
+//     try {
+//       const db = getDb();
 
-    return db
-      .collection("tickets")
-      .deleteOne({ _id: new ObjectId(id) })
-      .then(() => console.log("ticket deleted successfully"))
-      .catch((err) => err);
-  }
+//       return await db
+//         .collection("tickets")
+//         .find({ teamId: new ObjectId(teamId) })
+//         .toArray();
+//     } catch (err) {
+//       console.error("Database Fetch Error:", err);
+//       throw err;
+//     }
+//   }
 
-  static getById(id) {
-    const db = getDb();
+//   static DeleteById(id) {
+//     const db = getDb();
 
-    return db
-      .collection("tickets")
-      .findOne({ _id: new ObjectId(id) })
-      .then((ticket) => ticket)
-      .catch((err) => error);
-  }
-  static updateById(id, updatedData) {
-    const db = getDb();
-    return db
-      .collection("tickets")
-      .findOneAndUpdate(
-        { _id: new ObjectId(id) },
-        { $set: updatedData },
-        { returnDocument: "after" },
-      )
-      .then((result) => result);
-  }
-}
+//     return db
+//       .collection("tickets")
+//       .deleteOne({ _id: new ObjectId(id) })
+//       .then(() => console.log("ticket deleted successfully"))
+//       .catch((err) => err);
+//   }
+
+//   static getById(id) {
+//     const db = getDb();
+
+//     return db
+//       .collection("tickets")
+//       .findOne({ _id: new ObjectId(id) })
+//       .then((ticket) => ticket)
+//       .catch((err) => err);
+//   }
+//   static updateById(id, updatedData) {
+//     const db = getDb();
+//     return db
+//       .collection("tickets")
+//       .findOneAndUpdate(
+//         { _id: new ObjectId(id) },
+//         { $set: updatedData },
+//         { returnDocument: "after" },
+//       )
+//       .then((result) => result);
+//   }
+// }
