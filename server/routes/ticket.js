7814e084a6c3456fb0d905e4adef66f8d3f37936
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-04 12:00:00 +0500

    refactor: update ticket routing definitions

--- server/routes/ticket.js
+++ server/routes/ticket.js
@@ -6,13 +6,18 @@ import {
   deleteTicket,
   getTicket,
   patchTicketStatus,
+  assignToUser,
 } from "../controllers/ticket.js";
 import { isAdmin, isMember } from "../middleware/role-check.js";
 
 const Router = express.Router();
 
 Router.route("/").get(getTickets).post(postTicket);
-Router.route("/:id").get(getTicket).patch(patchTicket).delete(deleteTicket);
+Router.route("/:id")
+  .get(getTicket)
+  .patch(patchTicket)
+  .delete(isAdmin, deleteTicket);
 Router.route("/:id/status").patch(isMember, patchTicketStatus);
+Router.route("/:id/assign").patch(isAdmin, assignToUser);
 
 export default Router;
