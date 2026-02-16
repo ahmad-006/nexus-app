d037dd1506fd3ff202314b5502a418d0a595c8e7
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-16 20:40:32 +0500

    feat: initialize client-side application scaffold with React and Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-700 ">
+        Nexus Dashboard
+      </h1>
+    </>
+  );
+}
+
+export default App;
