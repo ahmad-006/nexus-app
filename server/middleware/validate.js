a0a4d789b73a7986873c6070f4fab0e2e09c8ba7
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-17 22:19:30 +0500

    feat: Add express-validator and validation middleware

---
+++ server/middleware/validate.js
@@ -0,0 +1,8 @@
+import { validationResult } from "express-validator";
+
+export const validate = (req, res, next) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).json({ errors: error.array() });
+  }
+};
---
+++ server/middleware/validator.js
@@ -0,0 +1,69 @@
+import { body, check } from "express-validator";
+import { User } from "../models/user";
+
+export const userValidation = {
+  email: body("email")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("An email must have minimum of 5 characters")
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Please Enter a valid email")
+    .custom(async (value) => {
+      const user = await User.exists({ email: value });
+      if (!user) throw new Error("User not found");
+    }),
+  name: body("name")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("A name must have minimum of 5 characters"),
+  password: body("password")
+    .trim()
+    .isStrongPassword()
+    .withMessage(
+      "Password must be at least 8 characters long and include an uppercase letter, a number, and a special character.",
+    ),
+  confirmPassword: body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) throw new Error("Passwords do not match");
+    return true;
+  }),
+  token: check("token")
+    .isHexadecimal()
+    .isLength({ min: 64, max: 64 })
+    .withMessage("invalid Token"),
+};
+
+const ticketValidationBase = {
+  status: () =>
+    body("status")
+      .isIn(["TODO", "IN_PROGRESS", "DONE"])
+      .withMessage("Invalid status"),
+  assigneeId: () =>
+    body("assigneeId").isMongoId().withMessage("Invalid Assignee"),
+};
+
+export const ticketValidation = {
+  title: body("title")
+    .isLength({ min: 5, max: 100 })
+    .withMessage("Title must be between 5 and 100 characters"),
+  description: body("description")
+    .isLength({ min: 10 })
+    .withMessage("Title must be atleast 10 characters long"),
+  priority: body("priority")
+    .isIn(["LOW", "MEDIUM", "HIGH"])
+    .withMessage("Invalid Priority"),
+  reporterId: check("reporterId").isMongoId().withMessage("Invalid Reporter"),
+  teamId: check("teamId").isMongoId().withMessage("Invalid Team"),
+  get optionalStatus() {
+    return ticketValidationBase.status().optional({ values: "falsy" });
+  },
+  get createStatus() {
+    return ticketValidationBase.status().notEmpty();
+  },
+  optionalAssignee() {
+    return ticketValidationBase.assigneeId().optional({ values: "falsy" });
+  },
+  createAssignee() {
+    return ticketValidationBase.assigneeId().notEmpty();
+  },
+};
