bbd1d0bea286932528959c01e776eb89e505b4d0
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-09 11:55:09 +0500

    feat: setup user routes with proper validation and controllers

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,14 +5,25 @@ import {
   patchUserProfile,
   deleteUser,
   getTickets,
-  getUser,
+  getUserById,
 } from "../controllers/user.js";
+import { validate } from "../middleware/validate.js";
+import { userValidation } from "../middleware/validator.js";
 
 const userRouter = express.Router();
 
-userRouter.route("/me").get(getUser).patch(patchUserProfile).delete(deleteUser);
-userRouter.get("/me/teams", getTeams);
-userRouter.get("/me/ticket", getTickets);
-userRouter.get("/:userId", getUser);
+userRouter
+  .route("/me")
+  .get(getUser)
+  .patch(
+    [userValidation.name],
+    validate,
+    patchUserProfile,
+  )
+  .delete(deleteUser);
+
+userRouter.get("/me/teams", [userValidation.email], validate, getTeams);
+userRouter.get("/me/ticket", [userValidation.email], validate, getTickets);
+userRouter.get("/:userId", [userValidation.email], validate, getUserById);
 
 export { userRouter };
