7697e8f0116ee91d606906c23457f650f63b22cd
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-09 16:08:28 +0500

    fix: Add .js extension to user model import in validator middleware

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -1,5 +1,5 @@
 import { body, check } from "express-validator";
-import { User } from "../models/user";
+import { User } from "../models/user.js";
 
 export const authValidation = {
   email: body("email")
