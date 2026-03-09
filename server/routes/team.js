3220702c1637c3292951b5229f8777a47b48d5d0
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-09 10:51:31 +0500

    feat: Implemented validation for team endpoints

--- server/routes/team.js
+++ server/routes/team.js
@@ -5,10 +5,33 @@ import {
   postCreateTeam,
 } from "../controllers/team.js";
 import { isAdmin } from "../middleware/role-check.js";
+import { teamValidation } from "../middleware/validator.js";
+import { validate } from "../middleware/validate.js";
 
 const teamsRouter = express.Router();
 
-teamsRouter.route("/").post(postCreateTeam);
-teamsRouter.route("/:id/role").patch(isAdmin, patchPromoteToAdmin);
-teamsRouter.route("/:id/member").post(isAdmin, postAddMember);
+teamsRouter
+  .route("/")
+  .post(
+    isAdmin,
+    [teamValidation.name, teamValidation.owenerId],
+    validate,
+    postCreateTeam,
+  );
+teamsRouter
+  .route("/:id/role")
+  .patch(
+    isAdmin,
+    [teamValidation.userId, teamValidation.teamId],
+    validate,
+    patchPromoteToAdmin,
+  );
+teamsRouter
+  .route("/:id/member")
+  .post(
+    isAdmin,
+    [teamValidation.userId, teamValidation.teamId],
+    validate,
+    postAddMember,
+  );
 export { teamsRouter };
