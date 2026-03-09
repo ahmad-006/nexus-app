751325b46314043460f105ed8b53ca609221252d
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-09 10:50:20 +0500

    feat: Implemented validation for ticket route

--- server/routes/ticket.js
+++ server/routes/ticket.js
@@ -9,15 +9,50 @@ import {
   assignToUser,
 } from "../controllers/ticket.js";
 import { isAdmin, isMember } from "../middleware/role-check.js";
+import { ticketValidation } from "../middleware/validator.js";
+import { validate } from "../middleware/validate.js";
 
 const ticketRouter = express.Router();
+const ticketUpdateRules = [
+  ticketValidation.title,
+  ticketValidation.description,
+  ticketValidation.priority,
+  ticketValidation.teamId,
+  ticketValidation.optionalAssignee,
+  ticketValidation.optionalStatus,
+];
 
-ticketRouter.route("/").get(getTickets).post(postTicket);
-ticketRouter.route("/:id")
+ticketRouter
+  .route("/")
+  .get(getTickets)
+  .post(ticketUpdateRules, validate, postTicket);
+ticketRouter
+  .route("/:id")
   .get(getTicket)
-  .patch(isAdmin, patchTicket)
+  .patch(
+    isAdmin,
+    [
+      ticketValidation.title,
+      ticketValidation.description,
+      ticketValidation.priority,
+      ticketValidation.teamId,
+      ticketValidation.optionalAssignee,
+      ticketValidation.optionalStatus,
+    ],
+    validate,
+    patchTicket,
+  )
   .delete(isAdmin, deleteTicket);
-ticketRouter.route("/:id/status").patch(isMember, patchTicketStatus);
-ticketRouter.route("/:id/assign").patch(isAdmin, assignToUser);
+ticketRouter
+  .route("/:id/status")
+  .patch(
+    isMember,
+    [ticketValidation.createStatus],
+    validate,
+    patchTicketStatus,
+  );
+ticketRouter
+  .route("/:id/assign")
+  .patch(isAdmin, [ticketValidation.createAssignee], validate, assignToUser);
 
 export { ticketRouter };
