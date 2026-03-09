ccd47df559b995e0ed877561fbd463d22ae38e30
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-09 16:08:04 +0500

    refactor: Use AppError in validation middleware

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -1,9 +1,10 @@
 import { validationResult } from "express-validator";
+import { AppError } from "../util/appError.js";
 
 export const validate = (req, res, next) => {
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return res.status(422).json({ errors: error.array() });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new AppError(errors.array()[0].msg, 422));
   }
 
   next();
