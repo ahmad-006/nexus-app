11badf32e48f5ddefdbeaa3bfb9bd8e0fc2ae93f
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-09 16:09:50 +0500

    fix: Correct syntax errors in user routes and remove redundancy

--- server/routes/user.js
+++ server/routes/user.js
@@ -15,15 +15,11 @@ const userRouter = express.Router();
 userRouter
   .route("/me")
   .get(getUser)
-  .patch(
-    [userValidation.name, userValidation.userId],
-    validate,
-    patchUserProfile,
-  )
+  .patch([userValidation.name, userValidation.userId], validate, patchUserProfile)
   .delete(deleteUser);
 
-userRouter.get("/me/teams"[userValidation.userId], validate, getTeams);
-userRouter.get("/me/ticket"[userValidation.userId], validate, getTickets);
+userRouter.get("/me/teams", [userValidation.userId], validate, getTeams);
+userRouter.get("/me/ticket", [userValidation.userId], validate, getTickets);
 userRouter.get("/:userId", [userValidation.userId], validate, getUserById);
 
 export { userRouter };
