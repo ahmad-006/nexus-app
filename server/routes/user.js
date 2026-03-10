11eb20d0b31bce841867890ac726809a1a72c050
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-10 15:45:31 +0500

    feat: add profile image upload route and update user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,8 +4,10 @@ import {
   getTeams,
   patchUserProfile,
   deleteUser,
-  getTickets,
   getUserById,
+  getTickets,
+  uploadImage,
+  postImage,
 } from "../controllers/user.js";
 import { validate } from "../middleware/validate.js";
 import { userValidation } from "../middleware/validator.js";
@@ -15,11 +17,15 @@ const userRouter = express.Router();
 userRouter
   .route("/me")
   .get(getUser)
-  .patch([userValidation.name, userValidation.userId], validate, patchUserProfile)
+  .patch(
+    [userValidation.name, userValidation.userId],
+    validate,
+    patchUserProfile,
+  )
   .delete(deleteUser);
-
+userRouter.post("/me/image", uploadImage, postImage);
 userRouter.get("/me/teams", [userValidation.userId], validate, getTeams);
-userRouter.get("/me/ticket", [userValidation.userId], validate, getTickets);
+userRouter.get("/me/tickets", [userValidation.userId], validate, getTickets);
 userRouter.get("/:userId", [userValidation.userId], validate, getUserById);
 
 export { userRouter };
