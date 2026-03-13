d11faf9da2c5afe557ff00ef0be21ccd32d10d1d
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-13 15:00:00 +0500

    feat(user): secure routes with protect middleware and cleanup validators

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,21 +11,26 @@ import {
 } from "../controllers/userController.js";
 import { validate } from "../middleware/validate.js";
 import { userValidation } from "../middleware/validator.js";
+import { protect } from "../controllers/authController.js";
 
 const userRouter = express.Router();
+
+// Apply protect to all user routes
+userRouter.use(protect);
 
 userRouter
   .route("/me")
   .get(getUser)
   .patch(
-    [userValidation.name, userValidation.userId],
+    [userValidation.name],
     validate,
     patchUserProfile,
   )
   .delete(deleteUser);
+
 userRouter.post("/me/image", uploadImage, postImage);
-userRouter.get("/me/teams", [userValidation.userId], validate, getTeams);
-userRouter.get("/me/tickets", [userValidation.userId], validate, getTickets);
-userRouter.get("/:userId", [userValidation.userId], validate, getUserById);
+userRouter.get("/me/teams", getTeams);
+userRouter.get("/me/tickets", getTickets);
+userRouter.get("/:userId", getUserById);
 
 export { userRouter };
