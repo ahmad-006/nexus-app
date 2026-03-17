7e05c84b020454115961b34e5856a9507c17d2cb
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-17 14:56:51 +0500

    feat: standardize ticket management logic and identifier usage

--- server/routes/ticketRoutes.js
+++ server/routes/ticketRoutes.js
@@ -22,7 +22,6 @@ const ticketUpdateValidation = [
   ticketValidation.title,
   ticketValidation.description,
   ticketValidation.priority,
-  ticketValidation.teamId,
   ticketValidation.optionalAssignee,
   ticketValidation.optionalStatus,
 ];
@@ -32,9 +31,8 @@ ROUTES
 */
 
 /*
-  ?By TEAMID:
-  Creating a ticket
-  Getting all the tickets
+Getting all the tickets
+Creating a ticket
 */
 
 ticketRouter
@@ -43,7 +41,6 @@ ticketRouter
   .post(protect, isMember, ticketUpdateValidation, validate, postTicket);
 
 /*
-  ?By ID:
   Getting a ticket 
   Updating a ticket
   deleting a ticket 
@@ -52,19 +49,23 @@ ticketRouter
 ticketRouter
   .route("/:ticketId")
   .get(protect, getTicket) // Any logged in user can see a ticket if they have the ID
-  .patch(protect, isAdminOrReporter, ticketUpdateValidation, validate, patchTicket)
+  .patch(
+    protect,
+    isAdminOrReporter,
+    ticketUpdateValidation,
+    validate,
+    patchTicket,
+  )
   .delete(protect, isAdminOrReporter, deleteTicket);
 
 //Updating the ticket status
-ticketRouter
-  .route("/:ticketId/status")
-  .patch(
-    protect,
-    isMember, // This needs a team context, we might need a custom check here if teamId isn't in URL
-    [ticketValidation.createStatus],
-    validate,
-    patchTicketStatus,
-  );
+ticketRouter.route("/:ticketId/status").patch(
+  protect,
+  isMember, // This needs a team context, we might need a custom check here if teamId isn't in URL
+  [ticketValidation.createStatus],
+  validate,
+  patchTicketStatus,
+);
 
 //Assigning a ticket
 ticketRouter
