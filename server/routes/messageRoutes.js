1e41e6f2c9c83a22436ae26c8ea394265d65466a
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-25 15:25:18 +0500

    feat(routes): register message editing and read-receipt endpoints

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { protect } from "../controllers/authController.js";
-import { getMessagesByUsers } from "../controllers/messageController.js";
+import {
+  getMessagesByUsers,
+  readAllMessages,
+  updateMessage,
+} from "../controllers/messageController.js";
 
 const messageRouter = express.Router();
 
 messageRouter.use(protect);
 
 messageRouter.get("/:otherUserId", getMessagesByUsers);
+messageRouter.patch("/read-all/:senderId", readAllMessages);
+messageRouter.patch("/:messageId", updateMessage);
 
 export { messageRouter };
