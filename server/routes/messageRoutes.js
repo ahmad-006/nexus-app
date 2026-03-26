b5ed11151cbfee85e3e9785f5747d73ac38b241a
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-03-26 16:56:10 +0500

    feat(messaging): implement siloed private and group messaging architecture

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -4,13 +4,16 @@ import {
   getMessagesByUsers,
   readAllMessages,
   updateMessage,
+  getMessagesByTeam,
 } from "../controllers/messageController.js";
+import { isMember } from "../middleware/role-check.js";
 
 const messageRouter = express.Router();
 
 messageRouter.use(protect);
 
-messageRouter.get("/:otherUserId", getMessagesByUsers);
+messageRouter.get("/:teamId/group", isMember, getMessagesByTeam);
+messageRouter.get("/:teamId/:otherUserId", isMember, getMessagesByUsers);
 messageRouter.patch("/read-all/:senderId", readAllMessages);
 messageRouter.patch("/:messageId", updateMessage);
 
